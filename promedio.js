e10b02103706320f4db83a487cbd17dc6f4a8b4c
KarenRivadero <rivaderokaren8@gmail.com>
2023-05-21 17:38:25 -0300

    Practico N0 finalizado

---
+++ promedio.js
@@ -0,0 +1,8 @@
+let num1 = -1.2
+let num2 = 5.3
+let num3 = -3.3
+let promedio = (num1 * num2 * num3) / 3
+console.log(promedio)
+//respuesta a = 105
+//respuesta b = -16
+//respuesta c = 6.995999999
