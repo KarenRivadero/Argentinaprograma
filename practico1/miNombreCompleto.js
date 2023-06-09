25a877f3667df55d6e5173e6ce2659c1fab5e5df
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-08 21:31:28 -0300

    subir carpeta

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -2,4 +2,4 @@ let nombre;
 nombre = "Karen";
 let apellido = "Rivadero";
 let edad = 27;
-console.log("Hola," + " " + nombre + " " + apellido + ". Tienes" + " " + edad + "años.");
+console.log("Hola," + " " + nombre + " " + apellido + ". Tienes" + " " + edad + " " + "años.");
