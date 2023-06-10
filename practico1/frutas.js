e3cb5ca786caa17def0af419ddb724709bc1ac32
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-10 11:24:37 -0300

    practico1 terminado

---
+++ practico1/frutas.js
@@ -0,0 +1,7 @@
+let frutas = []
+frutas = ["mandarina","frutilla","sandia","pera","banana"];
+console.log(frutas);
+let ultima = frutas.slice(4);
+console.log("La ultima fruta agregada fue" + " " + ultima);
+let primera = frutas.shift();
+console.log("La primera fruta agregada fue" + " " + primera);
