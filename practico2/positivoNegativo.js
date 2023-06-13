7e1d795cd5fd5065c24fb48729819fde4c0ffa36
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-13 20:47:50 -0300

    corregido

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,11 @@
+const readlineSync = require('readline-sync');
+const numero = readlineSync.question('Ingrese un numero: ');
+if (numero > 0){
+    console.log('El numero es positivo')
+} else if (numero == 0) {
+    console.log('El número es 0')
+}
+else {
+    console.log('El numero es negativo')
+};
+
