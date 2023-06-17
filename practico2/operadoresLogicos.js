eb05b044b8560c0846147dd67a7f44298fde1478
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-16 22:32:28 -0300

    switch case corregido

---
+++ practico2/operadoresLogicos.js
@@ -0,0 +1,16 @@
+const readlineSync = require('readline-sync');
+const numeroEntero = readlineSync.question('Ingrese un numero entero: ');
+
+if ((numeroEntero == 0) && (numeroEntero % 2 == 0)) {
+    console.log('El numero es positivo y par');
+}
+else if ((numeroEntero == 0) && (numeroEntero % 2 !== 0)){
+    console.log('El numero es positivo e impar')
+}
+else if (numeroEntero < 0){
+    console.log('El numero es negativo')
+}
+else if (numeroEntero == 0){
+    console.log('El numero es igual a 0')
+};
+
