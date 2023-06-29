6aa82601074a9de6006413cb084431eb254eb864
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-29 17:52:49 -0300

    terminado

--- practico3/listaDeInvitados.js
+++ practico3/listaDeInvitados.js
@@ -20,3 +20,28 @@ for (let i = 0; i < rechazados.length ; i++) {
     console.log("La lista de invitados rechazados es: " + rechazados[i]);
 }
 
+for (let i = 0; i < admitidos.length ; i++) {
+    admitidos.sort((a,b) => {
+        if (a === b) {
+            return 0;
+        }
+        if (a < b) {
+            return -1;
+        }
+        return 1;
+    })
+    console.log("La lista ordenada de invitados admitidos es: " + admitidos[i]);
+}
+
+for (let i = 0; i < rechazados.length ; i++) {
+    rechazados.sort((a,b) => {
+        if (a === b) {
+            return 0;
+        }
+        if (a < b) {
+            return -1;
+        }
+        return 1;
+    })
+    console.log("La lista ordenada de invitados rechazados es: " + rechazados[i]);
+}
