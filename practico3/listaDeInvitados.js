3045d39c463f0a25be0197f278f11deae49dd498
KarenRivadero <rivaderokaren8@gmail.com>
2023-06-29 15:56:25 -0300

    corregido

--- practico3/listaDeInvitados.js
+++ practico3/listaDeInvitados.js
@@ -2,14 +2,21 @@ let personas = ["Jose", "Maria", "Juan", "Andres", "Lionel", "Sofia", "Leandro",
 let rechazados = [];
 let admitidos = [];
 for (let i = 0; i < personas.length; i++) { 
-    if (i === "Sofia" || i === "Jose"){
-       rechazados += personas[i] + " ,";
-
+    if (personas[i] === "Sofia" || personas[i] === "Jose"){
+        rechazados.push(personas[i]);
     }
+    
     else {
-        admitidos += personas[i] + " ,";
+        admitidos.push(personas[i]);
     }
-    console.log(personas[i])
+    console.log(rechazados);
+    console.log(admitidos);
+}
+for (let i = 0; i < admitidos.length ; i++) {
+    console.log("La lista de invitados admitidos es: " + admitidos[i]);
 }
 
+for (let i = 0; i < rechazados.length ; i++) {
+    console.log("La lista de invitados rechazados es: " + rechazados[i]);
+}
 
